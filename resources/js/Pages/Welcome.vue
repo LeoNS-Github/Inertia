aeb07a6dfd3ae18380a5d3a598c98d5fea76b0c9
Linus <leonardonunes.link@gmail.com>
2022-12-13 22:48:39 -0300

    Chirper

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -10,11 +10,9 @@ defineProps({
 </script>
 
 <template>
-    <Head title="Welcome" />
+    <Head title="teste" />
 
-    <div
-        class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center sm:pt-0"
-    >
+    <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center sm:pt-0">
         <div v-if="canLogin" class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
             <Link
                 v-if="$page.props.auth.user"
